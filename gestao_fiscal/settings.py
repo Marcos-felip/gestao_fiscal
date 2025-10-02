7268bf6a06cd4213d6079ad147f2d18a3085beb3
Marcos <marcos.fernandes@mupisystems.com.br>
2025-10-01 23:37:54 -0300

    feat: adicionar configuração do admin_tabler e atualizar URLs do dashboard

--- gestao_fiscal/settings.py
+++ gestao_fiscal/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'crispy_forms',
     'crispy_bootstrap5',
+    'admin_tabler.apps.AdminTablerConfig',
 ]
 
 MIDDLEWARE = [
--- gestao_fiscal/urls.py
+++ gestao_fiscal/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', include('accounts.urls')),
-    path('', include('dashboard.urls')),
+    path('', include('admin_tabler.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ] + debug_toolbar_urls()
