486ed4507618f9347be5e9014e0576cf6d40989a
Marcos <marcos.fernandes@mupisystems.com.br>
2025-10-01 21:08:41 -0300

    Adicionar validação de número de telefone internacional usando intl-tel-input

---
+++ static/js/phone.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    const errorMap = [
+        "Número inválido",
+        "Código de país inválido",
+        "Numero incompleto",
+        "Numero muito comprido",
+        "Número inválido"
+    ];
+
+    const reset = (element_name) => {
+        $("#error-msg-" + element_name).html("");
+        $('#error-msg-' + element_name).prop("hidden", true);
+        $('#valid-msg-' + element_name).prop("hidden", true);
+    };
+
+    $(".international_phone").each(function() {
+        var element_name = $(this).attr("name");
+        var input = document.querySelector("#id_" + element_name);
+        var iti = window.intlTelInput(input, {
+            utilsScript: '/static/intl_tel_input/js/utils.js',
+            initialCountry: "br",
+            nationalMode: false,
+            separateDialCode: true,
+            preferredCountries: ['br'],
+        });
+
+        input.addEventListener('blur', () => {
+            reset(element_name);
+            if (input.value.trim()) {
+                if (iti.isValidNumber()) {
+                    $('#valid-msg-' + element_name).prop("hidden", false);
+                } else {
+                    const errorCode = iti.getValidationError();
+                    $('#error-msg-' + element_name).html(errorMap[errorCode]);
+                    $('#error-msg-' + element_name).prop("hidden", false);
+                }
+            }
+        });
+
+        input.addEventListener('change', reset(element_name));
+        input.addEventListener('keyup', reset(element_name));
+    });
+});
