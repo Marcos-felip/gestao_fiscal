10cbc2a8a74d1d618dc80868001d2c5d650f8435
Marcos-Fernandess <marcos.fernandes@mupisystems.com.br>
2025-10-02 13:26:12 -0300

    feat: remover referências ao admin_tabler e ajustar links no menu de navegação

--- gestao_fiscal/settings.py
+++ gestao_fiscal/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'crispy_forms',
     'crispy_bootstrap5',
-    'admin_tabler.apps.AdminTablerConfig',
 ]
 
 MIDDLEWARE = [
--- gestao_fiscal/urls.py
+++ gestao_fiscal/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', include('accounts.urls')),
-    path('', include('admin_tabler.urls')),
     path('dashboard/', include('dashboard.urls')),
 ] + debug_toolbar_urls()
