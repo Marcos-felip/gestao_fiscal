d302d06d552078f5f8b69b1df6c6bbc6951dd4e7
Marcos <marcos.fernandes@mupisystems.com.br>
2025-10-01 21:35:47 -0300

    feat: adicionar proteção de login à criação de empresa

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from accounts.views.company import CompanyCreateView
+from accounts.views import company
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('company/create/', CompanyCreateView.as_view(), name='company_create'),
+    path('company/create/', login_required(company.CompanyCreateView.as_view()), name='company_create'),
 ]
