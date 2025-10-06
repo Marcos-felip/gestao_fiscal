f74c9efa7a7900580a550277f747c9801f0b6475
Marcos-Fernandess <marcos.fernandes@mupisystems.com.br>
2025-10-06 18:54:17 -0300

    feat: adicionar página de perfill

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from accounts.views import company
+from accounts.views import company, profile
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('company/create/', login_required(company.CompanyCreateView.as_view()), name='company_create'),
+    path('profile/', login_required(profile.ProfileView.as_view()), name='profile'),
 ]
