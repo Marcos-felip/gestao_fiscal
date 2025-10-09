18b345ec6e38f546d6ca78a4599663c950ac6a34
Marcos <marcos.fernandes@mupisystems.com.br>
2025-10-08 21:06:34 -0300

    feat: registrar modelos de usuário, empresa e endereço no admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts.models.user import User, Membership
+from accounts.models.company import Company, Establishment
+from accounts.models.address import Address
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Membership)
+admin.site.register(Company)
+admin.site.register(Establishment)
+admin.site.register(Address)
